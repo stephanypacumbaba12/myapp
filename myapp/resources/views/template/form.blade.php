abfea010f7dc467c4ffb787a053c97116cbeac4e
nat <nathaniel@gmail.com>
2024-03-20 13:29:04 +0800

    first commit

--- myapp/resources/views/template/form.blade.php
+++ myapp/resources/views/template/form.blade.php
@@ -25,6 +25,6 @@
             </main>
 
 		<script src="{{asset('account/js/jquery-3.3.1.min.js')}}"></script>
-		<script src="js/main.js"></script>
+		<script src="{{asset('account/js/main.js')}}"></script>
 	</body>
 </html>
